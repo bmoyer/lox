c5b8a3d511cb68e4c18a2daf30459c6c542b9a5b
Ben Moyer <ben.s.moyer@gmail.com>
2018-09-13 05:04:16 +0000

    support scope with {}

--- lox/com/craftinginterpreters/lox/Environment.java
+++ lox/com/craftinginterpreters/lox/Environment.java
@@ -4,12 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Environment {
+    final Environment enclosing;
     private final Map<String, Object> values = new HashMap();
+
+    // constructor for global scope's environment
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
+        
+        if (enclosing != null) { return enclosing.get(name); }
 
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.'");
@@ -19,6 +31,11 @@ class Environment {
         if (values.containsKey(name.lexeme)) {
             values.put(name.lexeme, value);
             return;
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return; // TODO: can this return be removed?
         }
 
         throw new RuntimeError(name,
